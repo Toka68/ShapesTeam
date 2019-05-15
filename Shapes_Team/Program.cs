03ab07bf9f698b6960d0bc5bc7e54baf682157ba
hasham7861 <hasham.alam@hotmail.com>
2019-05-15 10:39:09 -0400

    Add project files.

---
+++ Shapes_Team/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes_Team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Intialize of ShapesTeam");
+        }
+    }
+}
